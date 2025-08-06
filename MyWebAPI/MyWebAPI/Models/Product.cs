a607a568ae2bf3dd15b3198050992c07fd553613
Dong2003 <dongnguyen17092003@gmail.com>
2025-08-06 16:40:43 +0700

    update 11

---
+++ MyWebAPI/MyWebAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tên sản phẩm không được để trống")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double DonGia { get; set; }
+    }
+}
